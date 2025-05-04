5c70f5cf9d2d1d143129331e78a0470d706827f8
Erwin Kramer <r3verse@gmail.com>
2025-05-04 11:18:05 +0200

    add TUnit

--- IntegrationTest.Console/Program.cs
+++ IntegrationTest.Console/Program.cs
@@ -5,31 +5,39 @@ using OpenTelemetry.Metrics;
 using OpenTelemetry.Trace;
 using System.Diagnostics;
 
-if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("AZURE_TENANT_ID")) ||
-    string.IsNullOrEmpty(Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING")))
-    throw new InvalidOperationException("Required environment variables 'AZURE_TENANT_ID' and/or 'APPLICATIONINSIGHTS_CONNECTION_STRING' are not set.");
-
-var servicebusNamespace = Environment.GetEnvironmentVariable("AZURE_SERVICEBUS_FULLYQUALIFIEDNAMESPACE")
-    ?? throw new InvalidOperationException("Environment variable 'AZURE_SERVICEBUS_FULLYQUALIFIEDNAMESPACE' is not set.");
-
-var serviceName = "Tomato Pricer";
-var cred = new DefaultAzureCredential(new DefaultAzureCredentialOptions { });
-
-using ActivitySource businessActivitySource = new ActivitySource("Guanchen.InternalServices.Demo.TomatoService", "2.0.0");
-using var loggerFactory = BusinessTracing.CreateLoggerFactory();
-using var meterProvider = BusinessTracing.CreateBusinessMetricProviderBuilder(serviceName).Build();
-using var tracerProvider = businessActivitySource.CreateBusinessTracerProviderBuilder(serviceName).Build();
-using var httpClient = new HttpClient();
-
-var logger = loggerFactory.CreateLogger(businessActivitySource);
-
-try
+public class Program
 {
-    await Common.ProcessTomatoBatchAsync(businessActivitySource, logger, httpClient, cred, servicebusNamespace);
-}
-catch (Exception ex)
-{
-    logger.LogError(ex, "Something went wrong while processing tomatoes.");
-}
+    public static async Task Main(string[] args)
+    {
+        var throwUnhandledException = args.Length > 0 && args[0] == "true";
 
-Console.WriteLine("Completed 😎");
+        if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("AZURE_TENANT_ID")) ||
+            string.IsNullOrEmpty(Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING")))
+            throw new InvalidOperationException("Required environment variables 'AZURE_TENANT_ID' and/or 'APPLICATIONINSIGHTS_CONNECTION_STRING' are not set.");
+
+        var servicebusNamespace = Environment.GetEnvironmentVariable("AZURE_SERVICEBUS_FULLYQUALIFIEDNAMESPACE")
+            ?? throw new InvalidOperationException("Environment variable 'AZURE_SERVICEBUS_FULLYQUALIFIEDNAMESPACE' is not set.");
+
+        var serviceName = "Tomato Pricer";
+        var cred = new DefaultAzureCredential(new DefaultAzureCredentialOptions { });
+
+        using ActivitySource businessActivitySource = new ActivitySource("Guanchen.InternalServices.Demo.TomatoService", "2.0.0");
+        using var loggerFactory = BusinessTracing.CreateLoggerFactory();
+        using var meterProvider = BusinessTracing.CreateBusinessMetricProviderBuilder(serviceName).Build();
+        using var tracerProvider = businessActivitySource.CreateBusinessTracerProviderBuilder(serviceName).Build();
+        using var httpClient = new HttpClient();
+
+        var logger = loggerFactory.CreateLogger(businessActivitySource);
+
+        try
+        {
+            await Common.ProcessTomatoBatchAsync(businessActivitySource, logger, httpClient, cred, servicebusNamespace);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Something went wrong while processing tomatoes.");
+        }
+
+        Console.WriteLine("Completed 😎");
+    }
+}
---
+++ IntegrationTest/Tests.cs
@@ -0,0 +1,16 @@
+﻿namespace IntegrationTest;
+
+public class Test
+{
+    [Before(Class)]
+    public static Task CreateContext()
+    {
+        return Task.CompletedTask;
+    }
+
+    [Test]
+    public async Task CreateBankReturnsCreated()
+    {
+        await Program.Main(["true"]); // Pass "true" to simulate an unhandled exception
+    }
+}
